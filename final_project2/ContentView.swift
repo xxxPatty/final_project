c2e82d27f1ff78b50ccf998242b0e81fe18d3880
Patty <L224749391@gmail.com>
2021-01-01 14:28:58 +0800

    Initial Commit

---
+++ final_project2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  final_project2
+//
+//  Created by 林湘羚 on 2021/1/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ final_project2/final_project2App.swift
@@ -0,0 +1,17 @@
+//
+//  final_project2App.swift
+//  final_project2
+//
+//  Created by 林湘羚 on 2021/1/1.
+//
+
+import SwiftUI
+
+@main
+struct final_project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
